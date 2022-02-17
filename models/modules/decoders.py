502366f7a71fb93cb2edea1a59c9ec29593ac99c
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-02-17 08:17:10 +0700

    refined the decoders.py

--- models/modules/decoders.py
+++ models/modules/decoders.py
@@ -1,28 +1,55 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-import numpy as np
 
 from models.modules.attentions import MultiHeadAttention
-from models.utils import sinusoid_encoding_table
+from models.utils import generate_sequential_mask, sinusoid_encoding_table, generate_padding_mask
 from models.modules.positionwise_feed_forward import PositionWiseFeedForward
 from models.modules.containers import Module, ModuleList
 
-class MeshedDecoderLayer(Module):
+class DecoderLayer(Module):
+    "Decoder is made of self-attn, src-attn, and feed forward (defined below)"
     def __init__(self, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1, self_att_module=None,
-                 enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
-        super(MeshedDecoderLayer, self).__init__()
-        self.self_att = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=True,
-                                           attention_module=self_att_module,
-                                           attention_module_kwargs=self_att_module_kwargs)
-        self.enc_att = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=False,
-                                          attention_module=enc_att_module,
-                                          attention_module_kwargs=enc_att_module_kwargs)
+                 use_aoa=False, enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
+        super(DecoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=True,
+                                            use_aoa=use_aoa,
+                                            attention_module=self_att_module,
+                                            attention_module_kwargs=self_att_module_kwargs)
+        self.enc_attn = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=False,
+                                            use_aoa=use_aoa,
+                                            attention_module=enc_att_module,
+                                            attention_module_kwargs=enc_att_module_kwargs)
+
         self.pwff = PositionWiseFeedForward(d_model, d_ff, dropout)
 
-        self.fc_alpha1 = nn.Linear(d_model + d_model, d_model)
-        self.fc_alpha2 = nn.Linear(d_model + d_model, d_model)
-        self.fc_alpha3 = nn.Linear(d_model + d_model, d_model)
+    def forward(self, input, enc_output, mask_pad, mask_self_att, mask_enc_att):
+        self_att = self.self_att(input, input, input, mask_self_att)
+        self_att = self_att * mask_pad
+
+        enc_att = self.enc_att(self_att, enc_output, enc_output, mask_enc_att) * mask_pad
+        enc_att = enc_att * mask_pad
+
+        ff = self.pwff(enc_att)
+        
+        return ff
+
+class MeshedDecoderLayer(Module):
+    def __init__(self, N_enc, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1, self_att_module=None,
+                 use_aoa=False, enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
+        super(MeshedDecoderLayer, self).__init__()
+        self.self_att = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=True,
+                                            use_aoa=use_aoa,
+                                            attention_module=self_att_module,
+                                            attention_module_kwargs=self_att_module_kwargs)
+        self.enc_att = MultiHeadAttention(d_model, d_k, d_v, h, dropout, can_be_stateful=False,
+                                            use_aoa=use_aoa,
+                                            attention_module=enc_att_module,
+                                            attention_module_kwargs=enc_att_module_kwargs)
+        self.pwff = PositionWiseFeedForward(d_model, d_ff, dropout)
+
+        self.N_enc = N_enc
+        self.fc_alphas = nn.ModuleList([nn.Linear(d_model + d_model, d_model) for _ in range(N_enc)])
 
         self.init_weights()
 
@@ -35,34 +62,41 @@ class MeshedDecoderLayer(Module):
         nn.init.constant_(self.fc_alpha3.bias, 0)
 
     def forward(self, input, enc_output, mask_pad, mask_self_att, mask_enc_att):
+        assert enc_output.size(1) == self.N_enc, "total layers of the encoder must equal to total number of the encoder outputs"
+        
         self_att = self.self_att(input, input, input, mask_self_att)
         self_att = self_att * mask_pad
 
-        enc_att1 = self.enc_att(self_att, enc_output[:, 0], enc_output[:, 0], mask_enc_att) * mask_pad
-        enc_att2 = self.enc_att(self_att, enc_output[:, 1], enc_output[:, 1], mask_enc_att) * mask_pad
-        enc_att3 = self.enc_att(self_att, enc_output[:, 2], enc_output[:, 2], mask_enc_att) * mask_pad
+        enc_atts = []
+        for ith in range(self.N_enc):
+            enc_atts.append(self.enc_att(self_att, enc_output[:, ith], enc_output[:, ith], mask_enc_att) * mask_pad)
 
-        alpha1 = torch.sigmoid(self.fc_alpha1(torch.cat([self_att, enc_att1], -1)))
-        alpha2 = torch.sigmoid(self.fc_alpha2(torch.cat([self_att, enc_att2], -1)))
-        alpha3 = torch.sigmoid(self.fc_alpha3(torch.cat([self_att, enc_att3], -1)))
+        alphas = []
+        for fc_alpha, enc_att in zip(self.fc_alphas, enc_atts):
+            alphas.append(torch.sigmoid(fc_alpha(torch.cat([self_att, enc_att], -1))))
 
-        enc_att = (enc_att1 * alpha1 + enc_att2 * alpha2 + enc_att3 * alpha3) / np.sqrt(3)
+        attn = 0
+        for alpha, enc_att in zip(alphas, enc_atts):
+            attn += enc_att * alpha
+        attn = attn / torch.sqrt(self.N_enc)
         enc_att = enc_att * mask_pad
 
-        ff = self.pwff(enc_att)
+        ff = self.pwff(attn)
         ff = ff * mask_pad
+
         return ff
 
-
-class MeshedDecoder(Module):
+class Decoder(Module):
+    "Generic N layer decoder with masking."
     def __init__(self, vocab_size, max_len, N_dec, padding_idx, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1,
-                 self_att_module=None, enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
-        super(MeshedDecoder, self).__init__()
+                 use_aoa=False, self_att_module=None, enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
+        super(Decoder, self).__init__()
+        
         self.d_model = d_model
         self.word_emb = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
         self.pos_emb = nn.Embedding.from_pretrained(sinusoid_encoding_table(max_len + 1, d_model, 0), freeze=True)
         self.layers = ModuleList(
-            [MeshedDecoderLayer(d_model, d_k, d_v, h, d_ff, dropout, self_att_module=self_att_module,
+            [DecoderLayer(d_model, d_k, d_v, h, d_ff, dropout, self_att_module=self_att_module, use_aoa=use_aoa,
                                 enc_att_module=enc_att_module, self_att_module_kwargs=self_att_module_kwargs,
                                 enc_att_module_kwargs=enc_att_module_kwargs) for _ in range(N_dec)])
         self.fc = nn.Linear(d_model, vocab_size, bias=False)
@@ -76,25 +110,66 @@ class MeshedDecoder(Module):
     def forward(self, input, encoder_output, mask_encoder):
         # input (b_s, seq_len)
         b_s, seq_len = input.shape[:2]
-        mask_queries = (input != self.padding_idx).unsqueeze(-1).float()  # (b_s, seq_len, 1)
-        mask_self_attention = torch.triu(torch.ones((seq_len, seq_len), dtype=torch.uint8, device=input.device),
-                                         diagonal=1)
+        mask_queries = generate_padding_mask(input, self.padding_idx)  # (b_s, seq_len)
+        mask_self_attention = generate_sequential_mask(seq_len)
         mask_self_attention = mask_self_attention.unsqueeze(0).unsqueeze(0)  # (1, 1, seq_len, seq_len)
-        mask_self_attention = mask_self_attention + (input == self.padding_idx).unsqueeze(1).unsqueeze(1).byte()
-        mask_self_attention = mask_self_attention.gt(0)  # (b_s, 1, seq_len, seq_len)
+        mask_self_attention = mask_self_attention or mask_queries.unsqueeze(1).unsqueeze(1)
         if self._is_stateful:
             self.running_mask_self_attention = torch.cat([self.running_mask_self_attention, mask_self_attention], -1)
             mask_self_attention = self.running_mask_self_attention
 
         seq = torch.arange(1, seq_len + 1).view(1, -1).expand(b_s, -1).to(input.device)  # (b_s, seq_len)
-        seq = seq.masked_fill(mask_queries.squeeze(-1) == 0, 0)
+        seq = seq.masked_fill(mask_queries, 0)
         if self._is_stateful:
             self.running_seq.add_(1)
             seq = self.running_seq
 
         out = self.word_emb(input) + self.pos_emb(seq)
-        for i, l in enumerate(self.layers):
-            out = l(out, encoder_output, mask_queries, mask_self_attention, mask_encoder)
+        for layer in self.layers:
+            out = layer(out, encoder_output, mask_queries.unsqueeze(-1), mask_self_attention, mask_encoder)
+
+        out = self.fc(out)
+        return F.log_softmax(out, dim=-1)
+
+class MeshedDecoder(Module):
+    def __init__(self, vocab_size, max_len, N_enc, N_dec, padding_idx, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1,
+                 use_aoa=False, self_att_module=None, enc_att_module=None, self_att_module_kwargs=None, enc_att_module_kwargs=None):
+        super(MeshedDecoder, self).__init__()
+        self.d_model = d_model
+        self.word_emb = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
+        self.pos_emb = nn.Embedding.from_pretrained(sinusoid_encoding_table(max_len + 1, d_model, 0), freeze=True)
+        self.layers = ModuleList(
+            [MeshedDecoderLayer(N_enc, d_model, d_k, d_v, h, d_ff, dropout, self_att_module=self_att_module, use_aoa=use_aoa,
+                                enc_att_module=enc_att_module, self_att_module_kwargs=self_att_module_kwargs,
+                                enc_att_module_kwargs=enc_att_module_kwargs) for _ in range(N_dec)])
+        self.fc = nn.Linear(d_model, vocab_size, bias=False)
+        self.max_len = max_len
+        self.padding_idx = padding_idx
+        self.N = N_dec
+
+        self.register_state('running_mask_self_attention', torch.zeros((1, 1, 0)).byte())
+        self.register_state('running_seq', torch.zeros((1,)).long())
+
+    def forward(self, input, encoder_output, mask_encoder):
+        # input (b_s, seq_len)
+        b_s, seq_len = input.shape[:2]
+        mask_queries = generate_padding_mask(input, self.padding_idx)  # (b_s, seq_len)
+        mask_self_attention = generate_sequential_mask(seq_len)
+        mask_self_attention = mask_self_attention.unsqueeze(0).unsqueeze(0)  # (1, 1, seq_len, seq_len)
+        mask_self_attention = mask_self_attention or mask_queries.unsqueeze(1).unsqueeze(1)
+        if self._is_stateful:
+            self.running_mask_self_attention = torch.cat([self.running_mask_self_attention, mask_self_attention], -1)
+            mask_self_attention = self.running_mask_self_attention
+
+        seq = torch.arange(1, seq_len + 1).view(1, -1).expand(b_s, -1).to(input.device)  # (b_s, seq_len)
+        seq = seq.masked_fill(mask_queries, 0)
+        if self._is_stateful:
+            self.running_seq.add_(1)
+            seq = self.running_seq
+
+        out = self.word_emb(input) + self.pos_emb(seq)
+        for layer in self.layers:
+            out = layer(out, encoder_output, mask_queries.unsqueeze(-1), mask_self_attention, mask_encoder)
 
         out = self.fc(out)
         return F.log_softmax(out, dim=-1)
--- models/modules/encoders.py
+++ models/modules/encoders.py
@@ -7,66 +7,66 @@ from models.utils import generate_padding_mask
 
 class EncoderLayer(nn.Module):
     def __init__(self, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1, identity_map_reordering=False,
-                 attention_module=None, attention_module_kwargs=None):
+                 use_aoa=False, attention_module=None, attention_module_kwargs=None):
         super(EncoderLayer, self).__init__()
         self.identity_map_reordering = identity_map_reordering
         self.mhatt = MultiHeadAttention(d_model, d_k, d_v, h, dropout, identity_map_reordering=identity_map_reordering,
+                                        use_aoa=use_aoa,
                                         attention_module=attention_module,
                                         attention_module_kwargs=attention_module_kwargs)
         self.pwff = PositionWiseFeedForward(d_model, d_ff, dropout, identity_map_reordering=identity_map_reordering)
 
-    def forward(self, queries, keys, values, attention_mask=None, attention_weights=None):
-        att = self.mhatt(queries, keys, values, attention_mask, attention_weights)
+    def forward(self, queries, keys, values, boxes=None, grid_size=None, attention_mask=None, attention_weights=None):
+        att = self.mhatt(queries, keys, values, boxes, grid_size, attention_mask, attention_weights)
         ff = self.pwff(att)
         return ff
 
 class Encoder(nn.Module):
     def __init__(self, N, padding_idx, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1,
-                 identity_map_reordering=False, attention_module=None, attention_module_kwargs=None):
+                 identity_map_reordering=False, use_aoa=False, attention_module=None, attention_module_kwargs=None):
         super(Encoder, self).__init__()
         self.d_model = d_model
         self.dropout = dropout
         self.layers = nn.ModuleList([EncoderLayer(d_model, d_k, d_v, h, d_ff, dropout,
                                                   identity_map_reordering=identity_map_reordering,
+                                                  use_aoa=use_aoa,
                                                   attention_module=attention_module,
                                                   attention_module_kwargs=attention_module_kwargs)
                                      for _ in range(N)])
         self.padding_idx = padding_idx
 
-    def forward(self, input, attention_weights=None):
+    def forward(self, input, boxes=None, grid_size=None, attention_weights=None):
         # input (b_s, seq_len, d_in)
         attention_mask = generate_padding_mask(input, self.padding_idx).unsqueeze(1).unsqueeze(1) # (bs, 1, 1, seq_len)
 
-        outs = []
         out = input
-        for l in self.layers:
-            out = l(out, out, out, attention_mask, attention_weights)
-            outs.append(out.unsqueeze(1))
+        for layer in self.layers:
+            out = layer(out, out, out, boxes, grid_size, attention_mask, attention_weights)
 
-        outs = torch.cat(outs, 1)
-        return outs, attention_mask
+        return out, attention_mask
 
 class MultiLevelEncoder(nn.Module):
     def __init__(self, N, padding_idx, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1,
-                 identity_map_reordering=False, attention_module=None, attention_module_kwargs=None):
+                 identity_map_reordering=False, use_aoa=False, attention_module=None, attention_module_kwargs=None):
         super(Encoder, self).__init__()
         self.d_model = d_model
         self.dropout = dropout
         self.layers = nn.ModuleList([EncoderLayer(d_model, d_k, d_v, h, d_ff, dropout,
                                                   identity_map_reordering=identity_map_reordering,
+                                                  use_aoa=use_aoa,
                                                   attention_module=attention_module,
                                                   attention_module_kwargs=attention_module_kwargs)
                                      for _ in range(N)])
         self.padding_idx = padding_idx
 
-    def forward(self, input, attention_weights=None):
+    def forward(self, input, boxes=None, grid_size=None, attention_weights=None):
         # input (b_s, seq_len, d_in)
         attention_mask = generate_padding_mask(input, self.padding_idx).unsqueeze(1).unsqueeze(1) # (bs, 1, 1, seq_len)
 
         outs = []
         out = input
-        for l in self.layers:
-            out = l(out, out, out, attention_mask, attention_weights)
+        for layer in self.layers:
+            out = layer(out, out, out, boxes, grid_size, attention_mask, attention_weights)
             outs.append(out.unsqueeze(1))
 
         outs = torch.cat(outs, 1)
