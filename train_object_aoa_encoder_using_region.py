7bd6d7e62489764fdd89dc3c227874ec4f5fb64f
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-03-23 07:01:31 +0700

    added result_utils module

--- train_object_aoa_encoder_using_region.py
+++ train_object_aoa_encoder_using_region.py
@@ -10,6 +10,8 @@ from models.modules.attentions import AugmentedGeometryScaledDotProductAttention
 from models.modules.encoders import Encoder
 from models.modules.decoders import Decoder
 
+from result_utils import *
+
 import torch
 from torch.utils import data
 from torch.optim import Adam
@@ -315,6 +317,10 @@ if __name__ == '__main__':
 
         if best:
             copyfile(os.path.join(config.checkpoint_path, config.model_name, "last_model.pth"), os.path.join(config.checkpoint_path, config.model_name, "best_val_model.pth"))
+            public_test_results = get_predictions_region_feature(model, public_test_dict_dataset, vocab=vocab)
+            convert_results(config.sample_public_test_json_path, public_test_results, split="public")
+            private_test_results = get_predictions_region_feature(model, private_test_dict_dataset, vocab=vocab)
+            convert_results(config.sample_private_test_json_path, private_test_results, split="private")
 
         if exit_train:
             break
