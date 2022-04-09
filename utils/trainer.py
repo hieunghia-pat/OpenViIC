d50736c58809082c6ffd189478797a0eb079d3de
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-04-09 15:39:43 +0700

    refined the Trainer class

--- utils/trainer.py
+++ utils/trainer.py
@@ -301,8 +301,6 @@ class Trainer:
 
             if switch_to_rl and not best:
                 checkpoint = self.load_checkpoint(os.path.join(config.checkpoint_path, config.model_name, "best_model.pth"))
-                print('Resuming from epoch %d, validation loss %f, best_val_cider %f, and best test_cider %f' % (
-                    checkpoint['epoch'], checkpoint['val_loss'], checkpoint['best_val_cider'], checkpoint['best_test_cider']))
 
             self.save_checkpoint({
                 'val_loss': val_loss,
