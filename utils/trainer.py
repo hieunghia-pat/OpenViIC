a76f84226a7a941d7c61cee2cf3c78f026b1f8ca
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-04-13 18:09:10 +0700

    refined the Trainer class

--- utils/trainer.py
+++ utils/trainer.py
@@ -401,6 +401,7 @@ class Trainer:
         for sample_item in tqdm(sample_json_data, desc="Converting results: "):
             for item in results:
                 if sample_item["id"] == item["filename"]:
-                    sample_item["captions"] = item["gens"].values()[0]
+                    generated_captions = list(item["gens"].values())
+                    sample_item["captions"] = generated_captions[0]
 
         json.dump(sample_json_data, open(os.path.join(config.checkpoint_path, config.model_name, f"{split}_results.json"), "w+"), ensure_ascii=False)
