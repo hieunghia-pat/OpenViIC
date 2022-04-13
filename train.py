04bc476520e5b1294f0b891ececaa03fd305634b
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-04-13 14:53:01 +0700

    refined the training process

--- train.py
+++ train.py
@@ -70,10 +70,10 @@ else:
 
 if config.sample_public_test_json_path is not None:
     public_results = trainer.get_predictions(public_test_dict_dataset)
-    json.dump(public_results, open("scored_public_results.json"), ensure_ascii=False)
+    json.dump(public_results, open(os.path.join(config.checkpoint_path, config.model_name, "scored_public_results.json")), ensure_ascii=False)
     trainer.convert_results(config.sample_public_test_json_path, public_results, split="public")
 
 if config.sample_private_test_json_path is not None:
     private_results = trainer.get_predictions(private_test_dict_dataset)
-    json.dump(private_results, open("scored_private_results.json"), ensure_ascii=False)
+    json.dump(private_results, open(os.path.join(config.checkpoint_path, config.model_name, "scored_private_results.json")), ensure_ascii=False)
     trainer.convert_results(config.sample_private_test_json_path, private_results, split="private")
