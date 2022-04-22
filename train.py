ec024df66c4aabb2df700ad31093744576e94cf0
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-04-22 22:43:15 +0700

    added tokenizer when defining the vocab

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ if not os.path.isdir(os.path.join(config.checkpoint_path, config.model_name)):
 
 # Creating vocabulary and dataset
 if not os.path.isfile(os.path.join(config.checkpoint_path, config.model_name, "vocab.pkl")):
-    vocab = Vocab([config.train_json_path, config.val_json_path])
+    vocab = Vocab([config.train_json_path, config.val_json_path], tokenizer=config.tokenizer)
     pickle.dump(vocab, open(os.path.join(config.checkpoint_path, config.model_name, "vocab.pkl"), "wb"))
 else:
     vocab = pickle.load(open(os.path.join(config.checkpoint_path, config.model_name, "vocab.pkl"), "rb"))
