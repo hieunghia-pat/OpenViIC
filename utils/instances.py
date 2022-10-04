172db34ae39c2bde064448a7df0e6b08ce0038d5
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-10-04 09:15:48 +0700

    updated getting predictions

--- utils/instances.py
+++ utils/instances.py
@@ -87,8 +87,6 @@ class Instances:
         for k in self._fields:
             if isinstance(self._fields[k], torch.Tensor):
                 return self._fields[k].shape[0]
-            if isinstance(self._fields[k], list):
-                return len(self._fields[k])
 
         return 0
 
