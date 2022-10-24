a3ff2c1e8422baf3d55a236715d7668094628f58
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-10-24 09:29:24 +0700

    added CamoTransformer from dev branch

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .modules import *
+from .standard_stransformer import (
+    StandardTransformerUsingGrid,
+    StandardTransformerUsingRegion
+)
+from .meshed_memory_transformer import MeshedMemoryTransformer
+from .object_relation_transformer import ObjectRelationTransformer
+from .unified_transformer import UnifiedTransformer
+from .camo_transformer import CamoTransformer
---
+++ trainers/__init__.py
@@ -0,0 +1,3 @@
+from .base_trainer import BaseTrainer
+from .en_trainer import enTrainer
+from .vi_trainer import viTrainer
