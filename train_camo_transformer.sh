bc67503f925c1f728033e79f627a7063f0da2e5a
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-10-27 22:54:35 +0700

    added training file for Camo Transformer

---
+++ train_camo_transformer.sh
@@ -0,0 +1,4 @@
+python3 train_transformer.py    --exp_name camo_transformer \
+                                --batch_size 60 \
+                                --features_path features/x152++_faster_rcnn \
+                                --annotation_folder annotations
