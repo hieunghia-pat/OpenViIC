a29c2bbb9eaebb7895f28ab4d560743dbd241282
Nghia Hieu Nguyen <19520178@gm.uit.edu.vn>
2022-12-06 22:35:07 +0700

    added training files for AoA and ORT

---
+++ train_aoa.sh
@@ -0,0 +1,5 @@
+python3 train_camo_aoa.py   --exp_name camo_aoa \
+                            --batch_size 60 \
+                            --features_path features/x152++_faster_rcnn \
+                            --annotation_folder annotations \
+                            --workers 2
---
+++ train_ort.sh
@@ -0,0 +1,5 @@
+python3 train_ort.py   --exp_name ort \
+                            --batch_size 60 \
+                            --features_path features/x152++_faster_rcnn \
+                            --annotation_folder annotations \
+                            --workers 2
